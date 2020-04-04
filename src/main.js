adb376fd6218c4fb9afda0d5847f3d1a333b9c0f
Alex <baks750@mail.ru>
2020-04-04 12:01:53 +0300

    Добавил файлы сборки в гитигнор и вынес контейнер board в отдельный модуль

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import {createMenuTemplate} from './components/menu.js';
 import {createFilterTemplate} from './components/filter.js';
+import {createBoardContainerTemplate} from './components/board-container.js';
 import {createSortingTemplate} from './components/sorting.js';
 import {createTaskEditorTemplate} from './components/task-editor.js';
 import {createTaskTemplate} from './components/task.js';
@@ -18,11 +19,12 @@ const render = (container, element, place = `beforeend`) => {
 
 render(mainControl, createMenuTemplate());
 render(main, createFilterTemplate());
-render(main, createSortingTemplate());
+render(main, createBoardContainerTemplate());
 
 const board = main.querySelector(`.board`);
-const boardTasks = board.querySelector(`.board__tasks`);
+render(board, createSortingTemplate(), `afterbegin`);
 
+const boardTasks = board.querySelector(`.board__tasks`);
 render(boardTasks, createTaskEditorTemplate());
 
 for (let i = 0; i < TASKS_COUNT; i++) {
